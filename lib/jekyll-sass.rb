43b83d69117b3c1fec6b81805a4fb08272678eb5
Jason Kozak <jay@noctal.com>
2012-11-19 23:48:34 -0500

    Improved configuration

--- lib/jekyll-sass.rb
+++ lib/jekyll-sass.rb
@@ -3,6 +3,17 @@ require "jekyll-sass/version"
 module Jekyll
   module Sass
     require 'sass'
+
+    class SassConfig
+      def self.get()
+        config = Hash["style", :compressed, "syntax", :scss]
+        if Jekyll.configuration({}).has_key?('sass')
+          config.merge!(Jekyll.configuration({})['sass']) {|key,v1,v2| v2.to_sym}
+        end
+        config
+      end
+    end
+
     class SassCssFile < StaticFile
 
       # Obtain destination path.
@@ -10,14 +21,16 @@ module Jekyll
       #
       # Returns destination file path.
       def destination(dest)
-        File.join(dest, @dir, @name.sub(/scss$/, 'css'))
+        syntax = SassConfig.get()['syntax'].to_s
+        File.join(dest, @dir, @name.sub(/#{syntax}$/, 'css'))
       end
 
-      # Convert the scss file into a css file.
+      # Convert the sass/scss file into a css file.
       #   +dest+ is the String path to the destination dir
       #
       # Returns false if the file was not modified since last time (no-op).
       def write(dest)
+        config = SassConfig.get()
         dest_path = destination(dest)
 
         return false if File.exist? dest_path and !modified?
@@ -25,11 +38,8 @@ module Jekyll
 
         FileUtils.mkdir_p(File.dirname(dest_path))
         begin
-          config = Jekyll.configuration({})
-          style = config.has_key?('sass_style') ? config['sass_style'].to_sym : :compressed
-
           content = File.read(path)
-          engine = ::Sass::Engine.new( content, :syntax => :scss, :load_paths => ["#{@site.source}#{@dir}"], :style => style )
+          engine = ::Sass::Engine.new( content, :syntax => config['syntax'], :load_paths => ["#{@site.source}#{@dir}"], :style => config['style'] )
           content = engine.render
           File.open(dest_path, 'w') do |f|
             f.write(content)
@@ -47,12 +57,13 @@ module Jekyll
     class SassCssGenerator < Generator
       safe true
 
-      # Jekyll will have already added the *.scss files as Jekyll::StaticFile
+      # Jekyll will have already added the *.sass/scss files as Jekyll::StaticFile
       # objects to the static_files array.  Here we replace those with a
       # SassCssFile object.
       def generate(site)
+        syntax = SassConfig.get()['syntax'].to_s
         site.static_files.clone.each do |sf|
-          if sf.kind_of?(Jekyll::StaticFile) && sf.path =~ /\.scss$/
+          if sf.kind_of?(Jekyll::StaticFile) && sf.path =~ /\.#{syntax}$/
             site.static_files.delete(sf)
             name = File.basename(sf.path)
             destination = File.dirname(sf.path).sub(site.source, '')
@@ -60,6 +71,8 @@ module Jekyll
           end
         end
       end
+
     end
+
   end
 end
